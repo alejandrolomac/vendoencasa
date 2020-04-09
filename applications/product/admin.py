270296b8d07dfeeeb8a540338e0f0399eb696f04
reikrad <reikrad@gmail.com>
2020-04-08 21:54:39 -0600

    0.1

--- applications/product/admin.py
+++ applications/product/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Company, Category, SubCategory, Products
 
-# Register your models here.
+class CompanyAdmin(admin.ModelAdmin):
+	list_display = (
+		'id',
+		'name',
+	)
+
+	search_fields = ( 'name', )
+
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = (
+		'id',
+		'name',
+	)
+
+class SubCategoryAdmin(admin.ModelAdmin):
+	list_display = (
+		'id',
+		'name',
+	)
+
+	list_filter = ( 'category', )
+
+class ProductsAdmin(admin.ModelAdmin):
+	list_display = (
+		'id',
+		'title',
+		'subCategory',
+		'company',
+	)
+
+	search_fields = ( 'title', 'company', )
+	list_filter = ( 'subCategory', )
+
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(SubCategory, SubCategoryAdmin)
+admin.site.register(Products, ProductsAdmin)
--- applications/product/models.py
+++ applications/product/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from django.utils.text import slugify
-from django.contrib.contenttypes.fields import GenericRelation
+from django.utils.text import slugify 
 
 class Company(models.Model):
 	name = models.CharField('Nombre', max_length=150, blank=False)
-	address = models.CharField('Direccion', blank=True)
+	address = models.CharField('Direccion', max_length=400, blank=True)
 	logo = models.ImageField('Logo', upload_to='company/% Y/% m/% d/', blank=True)
 	facebook = models.URLField('Facebook', blank=True)
 	twitter = models.URLField('Twitter', blank=True)
@@ -12,61 +11,58 @@ class Company(models.Model):
 	youtube = models.URLField('Youtube', blank=True)
 	website = models.URLField('WebSite', blank=True)
 	email = models.EmailField('E-Mail', blank=True)
-	phone = models.CharField('Telefono', blank=True)
+	phone = models.CharField('Telefono', max_length=15, blank=True)
 	slug = models.SlugField('Slug', blank=False, unique=True)
 	resume = models.TextField('Resumen', blank=True)
 
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super(Company, self).save(*args, **kwargs)
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Company, self).save(*args, **kwargs)
 
 	def __str__(self):
 		return self.name
 
-
 class Category(models.Model):
-	name = models.CharField('Nombre', max_length=100, unique=False)
+	name = models.CharField('Categoria', max_length=100, unique=False)
 	icono = models.ImageField('Icono', upload_to='category/% Y/% m/% d/', blank=True)
 	slug = models.SlugField('Slug', blank=False, unique=True)
 
 	def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super(Category, self).save(*args, **kwargs)
+		self.slug = slugify(self.name)
+		super(Category, self).save(*args, **kwargs)
 
-    def __str__(self):
-    	return self.name
-
+	def __str__(self):
+		return self.name
 
 class SubCategory(models.Model):
-	name = models.CharField('Nombre', max_length=100, unique=False)
+	name = models.CharField('Sub Categoria', max_length=100, unique=False)
 	icono = models.ImageField('Icono', upload_to='subcategory/% Y/% m/% d/', blank=True)
-	category = models.ForeignKey(Category)
+	category = models.ForeignKey(Category, models.SET_NULL, null=True)
 	slug = models.SlugField('Slug', blank=False, unique=True)
 
 	def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super(SubCategory, self).save(*args, **kwargs)
+		self.slug = slugify(self.name)
+		super(SubCategory, self).save(*args, **kwargs)
 
-    def __str__(self):
-    	return self.name
+	def __str__(self):
+		return self.name
 
-
-class Product(models.Model):
-	title = models.CharField('Titulo', blank=False)
-	company = models.ForeignKey('Empresa', Company, on_delete=models.CASCADE)
-	category = GenericRelation('Categoria', Category)
-	subCategory = GenericRelation('Sub Categoria', SubCategory)
+class Products(models.Model):
+	title = models.CharField('Titulo', max_length=300, blank=False)
+	company = models.ForeignKey(Company, on_delete=models.CASCADE)
+	subCategory = models.ForeignKey(SubCategory, models.SET_NULL, null=True)
 	resume = models.TextField('Descripcion', blank=True)
 	imagef = models.ImageField('Imagen 1', upload_to='company/% Y/% m/% d/', blank=True)
 	images = models.ImageField('Imagen 2', upload_to='company/% Y/% m/% d/', blank=True)
 	imaget = models.ImageField('Imagen 3', upload_to='company/% Y/% m/% d/', blank=True)
-	price = models.CharField('Precio', blank=False)
+	price = models.CharField('Precio', max_length=12, blank=False)
 	calification = models.IntegerField('Calificacion', blank=True)
 	slug = models.SlugField('Slug', blank=False, unique=True)
+	pub_date = models.DateField(auto_now=True)
 
 	def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super(Product, self).save(*args, **kwargs)
+		self.slug = slugify(self.title)
+		super(Products, self).save(*args, **kwargs)
 
 	def __str__(self):
 		return self.title
