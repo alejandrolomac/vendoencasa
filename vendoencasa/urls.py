b1ea29940279771303eb62a8f2c388dfe2b36f52
reikrad <reikrad@gmail.com>
2020-04-10 08:38:25 -0600

    0.2

--- vendoencasa/urls.py
+++ vendoencasa/urls.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
 	re_path(r'', include('applications.product.urls')),
     re_path('admin/', admin.site.urls),
-    re_path('media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
-    re_path('static/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.STATIC_ROOT}),
 ]
