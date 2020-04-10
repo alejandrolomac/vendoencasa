49015f94b919453cc12f880da2858c248bae6759
reikrad <reikrad@gmail.com>
2020-04-10 01:07:29 -0600

    0.2

--- vendoencasa/settings.py
+++ vendoencasa/settings.py
@@ -92,7 +92,8 @@ USE_TZ = True
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = "/var/www/vendoencasa.net/static/"
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
--- vendoencasa/urls.py
+++ vendoencasa/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
-
 urlpatterns = [
 	re_path(r'', include('applications.product.urls')),
     re_path('admin/', admin.site.urls),
