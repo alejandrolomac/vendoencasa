211da6794b794b6ad5195e4d228f9455649b4e2e
reikrad <reikrad@gmail.com>
2020-04-10 08:28:29 -0600

    0.2

--- vendoencasa/urls.py
+++ vendoencasa/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, re_path, include
 urlpatterns = [
 	re_path(r'', include('applications.product.urls')),
     re_path('admin/', admin.site.urls),
-    (r'^media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.STATIC_ROOT}),
 ]
