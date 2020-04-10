2108ca5d91e45076da4f4460ce4ad9b5ddd7921f
reikrad <reikrad@gmail.com>
2020-04-10 01:22:25 -0600

    0.2

--- vendoencasa/settings.py
+++ vendoencasa/settings.py
@@ -89,12 +89,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
 #STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_ROOT = "/var/www/vendoencasa.net/static/"
+#STATIC_ROOT = "/var/www/vendoencasa.net/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
