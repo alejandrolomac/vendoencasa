180c1b11d7d1fed7ac2f0a1bf3da3d496a3d754f
reikrad <reikrad@gmail.com>
2020-04-13 04:42:43 -0600

    0.6

--- static/js/slider.js
+++ static/js/slider.js
@@ -1,6 +1,5 @@
-$('#catscarousel').on('slide.bs.carousel', function (e) {
+$('#carouselExample').on('slide.bs.carousel', function (e) {
 
-  
     var $e = $(e.relatedTarget);
     var idx = $e.index();
     var itemsPerSlide = 4;
@@ -8,13 +7,4 @@ $('#catscarousel').on('slide.bs.carousel', function (e) {
     
     if (idx >= totalItems-(itemsPerSlide-1)) {
         var it = itemsPerSlide - (totalItems - idx);
-        for (var i=0; i<it; i++) {
-            if (e.direction=="left") {
-                $('.carousel-item').eq(i).appendTo('.carousel-inner');
-            }
-            else {
-                $('.carousel-item').eq(0).appendTo('.carousel-inner');
-            }
-        }
-    }
-});
+        for (var i=0; i
