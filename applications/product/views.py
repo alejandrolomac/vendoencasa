77cb9b5ec3f553850477b835fc800133a1f9be92
reikrad <reikrad@gmail.com>
2020-04-15 02:44:56 -0600

    0.7

--- applications/product/views.py
+++ applications/product/views.py
@@ -84,7 +84,7 @@ def search(request):
 	paginator = Paginator(results, 1)
 	page = request.GET.get('page')
 	contacts = paginator.get_page(page)
-	countsearch = results.objects.count()
+	countsearch = Products.objects.filter(results).count()
 	return render(request, 'search.html', {'contacts': contacts, 'querytext': query, 'countsearch': countsearch})
 
 
