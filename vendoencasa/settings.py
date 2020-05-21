1cd8934740f7a3e13ff353ca43f5ba2a1c31f2be
reikrad <reikrad@gmail.com>
2020-05-20 20:10:09 -0600

    1.1

--- vendoencasa/settings.py
+++ vendoencasa/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = '&mcng4k3l9@5y4&(8(4j*1$o022z%3(^v2hfe#y=!71@k@#ja$'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', 'https://www.vendoencasa.net', 'http://www.vendoencasa.net', 'www.vendoencasa.net', 'http://vendoencasa.herokuapp.com']
 
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'applications.dashboard',
     'storages',
     'mathfilters',
+    'sorl.thumbnail',
 ]
 
 MIDDLEWARE = [
