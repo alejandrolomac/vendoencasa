f2cfce2ed1780646b30b0d1b05d63380c7e904b2
reikrad <reikrad@gmail.com>
2020-08-25 23:46:51 -0600

    1.2

--- vendoencasa/settings.py
+++ vendoencasa/settings.py
@@ -178,7 +178,7 @@ LOGIN_REDIRECT_URL = 'product_app:index'
 SITE_ID = 2
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'vendoencasahn@gmail.com'
 EMAIL_HOST_PASSWORD = 'yvacem93'
